f81ccc2d93d83281f6f127f9c94cc5dba580a015
Kamalkannan G <kk23101998@gmail.com>
2026-07-29 18:06:21 +0530

    Add files via upload

---
+++ 1.Univariate/5.Histogram_Skew_Kurtosis/Univariate.py
@@ -0,0 +1,10 @@
+class Univariate:
+    def quanQual(dataset):
+        quan = []
+        qual = []
+        for colum in dataset.columns:
+            if dataset[colum].dtypes == "object":
+                qual.append(colum)
+            else:
+                quan.append(colum)
+        return quan, qual
